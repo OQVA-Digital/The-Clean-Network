285906e8af6164540fe0638210083212688bc57d
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2023-07-22 14:34:11 +0100

    Add files via upload

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Clean Network - Commercial & Office Cleaning Services';
-export const SITE_DESCRIPTION = 'COMMERCIAL & OFFICE CLEANING SERVICES The proof is in our numbers. We clean over 500 offices each day with our office cleaning services. 95% of our clients choose to stay with us at the end of their contract. View our Services Request your Quote Rated 5 out of 5 on Google Confidence guaranteed with over';
+export const SITE_TITLE = 'Professional Commercial & Office Cleaning in London - The Clean Network';
+export const SITE_DESCRIPTION = "The Clean Network, London's trusted partner in professional commercial and office cleaning. Reliable, high standards, tailored to your needs. See why 95% of our clients stay.";
 export const OG_IMAGE = '/og/social-image.jpg'
