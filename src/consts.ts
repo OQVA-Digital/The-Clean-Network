f1624a914375396bdf8f8f8e02fc1f244a0f4174
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2023-07-24 18:03:12 +0100

    Add files via upload

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,50 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// MAIN WEBSITE DATA
 
 export const SITE_TITLE = 'Professional Commercial & Office Cleaning in London - The Clean Network';
+
 export const SITE_DESCRIPTION = "The Clean Network, London's trusted partner in professional commercial and office cleaning. Reliable, high standards, tailored to your needs. See why 95% of our clients stay.";
+
+
+
+// SOCIAL SHARING IMAGE
+
 export const OG_IMAGE = '/og/social-image.jpg'
+
+
+
+// SERVICES
+
+export const SERVICES_TITLE = 'Commercial & Office Cleaning Services in London'
+
+export const SERVICES_DESCRIPTION = 'Explore a wide range of professional commercial and office cleaning services by The Clean Network. Tailored solutions to meet your specific cleaning needs in London.'
+
+
+
+// SECTORS
+
+export const SECTORS_TITLE = 'Our Sectors · The Clean Network'
+
+export const SECTORS_DESCRIPTION = 'Discover the range of commercial & office cleaning services offered by The Clean Network in London. Comprehensive, reliable, professional. Your sector, our expertise.'
+
+
+
+// CONTACT PAGE
+
+export const CONTACT_TITLE = 'Contact Us · The Clean Network'
+
+export const CONTACT_DESCRIPTION = ''
+
+
+
+// ABOUT US PAGE
+
+export const ABOUT_TITLE = 'About The Clean Network - Who We Are'
+
+export const ABOUT_DESCRIPTION = "Discover who we are at The Clean Network. Learn about our history, our commitment to excellence, and why we're London's trusted cleaning service."
+
+
+// JOBS PAGE
+
+export const JOBS_TITLE = 'Office Cleaning Jobs in London - The Clean Network'
+
+export const JOBS_DESCRIPTION = "Looking for office cleaning jobs in London? Join The Clean Network, London's leading commercial cleaning company. Explore our job opportunities near you. Apply now!"
