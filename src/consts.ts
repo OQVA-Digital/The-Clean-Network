6c8ba46da830e477c1de9dc894424350beca8851
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2023-07-24 21:02:16 +0100

    Add files via upload

--- src/consts.ts
+++ src/consts.ts
@@ -1,30 +1,108 @@
-// MAIN WEBSITE DATA
+// GENERAL
+
+export const FULL_ADDRESS = "6-8 Knights Court, London, EC1M 4NH"
+
+export const STREET = "6-8 Knights Court"
+
+export const LONDON_PHONE_NO = "020 3411 0275"
+
+export const LONDON_PHONE_NO_HREF = "+44-020-3411-0275"
+
+
+
+
+// HOME PAGE
+
+    // META
 
 export const SITE_TITLE = 'Professional Commercial & Office Cleaning in London - The Clean Network';
 
 export const SITE_DESCRIPTION = "The Clean Network, London's trusted partner in professional commercial and office cleaning. Reliable, high standards, tailored to your needs. See why 95% of our clients stay.";
 
+export const SITE_KEYWORDS = "commercial cleaning, office cleaning, London, services, company, professional, specialist, best, commercial/office cleaning services, London, corporate office buildings, cleaning services, London small offices, cleaning services, London medical and dental offices, cleaning services, London architects offices, cleaning services, London media agencies and designers offices, cleaning services, London legal practices, cleaning services, London technology companies, cleaning services, London financial service companies, cleaning services, London"
 
 
-// SOCIAL SHARING IMAGE
+    // CONTENT
+
+export const SITE_H1 = 'Commercial & Office Cleaning in London'
+
+export const INTRO_H2 = 'Welcome to <strong>The Clean Network</strong> - trusted by over 500 offices in London each day. Discover the difference our professional office cleaning services can make. <b>Why do 95% of our clients renew their contracts with us? <span class="highlight">Let us show you.</span></b>'
+
+export const TEXT_BLOCK = "We're more than a cleaning company. <b>We're your partners in creating a clean, healthy, and productive work environment.</b> Our professional team caters to various sectors in London, ensuring your specific needs are met with meticulous attention to detail, unmatched reliability, and high industry standards. <b>Experience the professionalism that keeps our clients coming back.</b>"
+
+export const SERVICES_SECTION_DESCRIPTION = "At The Clean Network, we offer a comprehensive range of services. From offices to commercial spaces, our team is equipped with the latest technology and training to deliver impeccable results."
+
+export const SECTORS_SECTION_DESCRIPTION = "We understand that each place has unique needs. That's why we specialise in providing tailored cleaning solutions to various sectors."
+
+export const LOCATIONS_SECTION_DESCRIPTION = "Centrally located in the City of London, we're just a call away from providing your space with the professional cleaning service it deserves. We’d be happy to come over and provide you with a quote. Or feel free to drop by our office too."
+
+// END OF HOME PAGE
+
+
+
+
+
+
+
+// SOCIAL SHARING IMAGE PATH
 
 export const OG_IMAGE = '/og/social-image.jpg'
 
 
 
+
+
+
+
 // SERVICES
 
-export const SERVICES_TITLE = 'Commercial & Office Cleaning Services in London'
+    // META
+
+export const SERVICES_TITLE = 'Commercial & Office Cleaning Services in London - The Clean Network'
 
 export const SERVICES_DESCRIPTION = 'Explore a wide range of professional commercial and office cleaning services by The Clean Network. Tailored solutions to meet your specific cleaning needs in London.'
+
+export const SERVICES_KEYWORDS = "INSERT CONTENT"
+
+
+    // CONTENT
+
+export const SERVICES_H1 = "Commercial & Office Cleaning Services"
+
+export const SERVICES_INTRO_DESCRIPTION = "<strong>Discover the breadth and depth of our cleaning expertise at The Clean Network.</strong> We offer a comprehensive range of interior and exterior commercial and office <strong>cleaning services in London</strong>, tailor-made to meet your unique needs and exceed your expectations."
+
+export const INTERIOR_SERVICES_DESCRIPTION = "Our interior cleaning services are meticulously designed to ensure a clean, hygienic, and productive workspace."
+
+export const EXTERIOR_SERVICES_DESCRIPTION = "We extend our cleaning expertise beyond your building's interior."
+
+
+
+
 
 
 
 // SECTORS
 
-export const SECTORS_TITLE = 'Our Sectors · The Clean Network'
+    // META
+
+export const SECTORS_TITLE = 'Commercial & Office Cleaning Services in London - The Clean Network'
 
 export const SECTORS_DESCRIPTION = 'Discover the range of commercial & office cleaning services offered by The Clean Network in London. Comprehensive, reliable, professional. Your sector, our expertise.'
+
+export const SECTORS_KEYWORDS = "INSERT CONTENT"
+
+
+    // CONTENT
+
+export const SECTORS_H1 = "Commercial & Office Cleaning Services in London"
+
+export const SECTORS_INTRO_DESCRIPTION = "<strong>The Clean Network</strong> is here for all your commercial and office cleaning needs in London. We take pride in our range of professional cleaning services, all <b>tailored to provide a clean, healthy, and productive work environment that leaves a lasting impression</b>"
+
+
+
+
+
+
 
 
 
@@ -32,19 +110,106 @@ export const SECTORS_DESCRIPTION = 'Discover the range of commercial & office cl
 
 export const CONTACT_TITLE = 'Contact Us · The Clean Network'
 
-export const CONTACT_DESCRIPTION = ''
+export const CONTACT_DESCRIPTION = 'INSERT CONTENT'
+
+export const CONTACT_KEYWORDS = "INSERT CONTENT"
+
+
+
+
 
 
 
 // ABOUT US PAGE
 
+    // META
+
 export const ABOUT_TITLE = 'About The Clean Network - Who We Are'
 
 export const ABOUT_DESCRIPTION = "Discover who we are at The Clean Network. Learn about our history, our commitment to excellence, and why we're London's trusted cleaning service."
 
+export const ABOUT_KEYWORDS = "INSERT CONTENT"
+
+
+    // CONTENT
+
+export const ABOUT_H1 = "About <strong>The Clean Network</strong>"
+
+export const ABOUT_H2 = "The Clean Network – Our Story, Our Values, Our Commitment"
+    
+export const ABOUT_TEXT_BODY = `<p>We are one of the leading office <strong>cleaning companies in London</strong>.</p>
+    
+<p>The Clean Network is your professional partner for commercial and office cleaning in London. Our story began with a singular mission - to provide businesses with <strong>reliable, high-standard cleaning services, instilling trust through our actions and maintaining a level of quality that speaks for itself.</strong></p>
+
+<p>Our philosophy is simple yet robust - cleanliness and productivity go hand in hand. A clean working environment not only enhances the well-being of your staff but also promotes efficiency, motivation, and a positive corporate image. <b>We have spent years perfecting our services and processes, continuously investing in our team to meet the evolving needs of businesses in London.</b></p>
+
+<p>Today, we are proud to be the trusted choice of over <b>500 offices daily, from small offices to corporate office buildings, legal practices to technology companies, and more.</b> Our cleaning specialists understand the unique needs of each sector, delivering tailored services with meticulous attention to detail and the <b>highest standards</b>.</p>
+
+<p>At The Clean Network, we believe in building strong, long-lasting relationships with our clients. <b>Our 95% contract renewal rate speaks volumes about the trust businesses place in our services and our consistent ability to deliver excellence.</b> We don’t just clean; we care for your workspace as if it were our own, bringing an unwavering commitment to cleanliness and hygiene to your door, every single day.</p>
+
+<p>Our team is our strength. Comprising skilled, dedicated professionals passionate about delivering top-tier service, we ensure every member of our team is thoroughly trained and equipped with the latest cleaning technology. As a company, <b>we are committed to providing a positive, supportive work environment where our team can excel</b>, reflecting our commitment to excellence in every task we undertake.</p>
+
+<p><strong class="highlight">Partner with us and discover the difference The Clean Network can make.</strong></p>`
+
+
 
 // JOBS PAGE
+
+    // META
 
 export const JOBS_TITLE = 'Office Cleaning Jobs in London - The Clean Network'
 
 export const JOBS_DESCRIPTION = "Looking for office cleaning jobs in London? Join The Clean Network, London's leading commercial cleaning company. Explore our job opportunities near you. Apply now!"
+
+export const JOBS_KEYWORDS = "INSERT CONTENT"
+
+
+    // CONTENT
+
+export const JOBS_H1 = "Office Cleaning Jobs"
+
+export const JOBS_TEXT_BODY = `<p>If you're seeking a rewarding career in commercial cleaning, you're in the right place. <strong>Based in London, we're proud to offer a range of job opportunities in office cleaning, right in your area.</strong></p>
+    
+<p>At <strong>The Clean Network</strong>, we firmly believe that our team is our strength, and we're always on the lookout for dedicated individuals who share our <b>commitment to cleanliness, professionalism, and customer service.</b> As part of our team, you'll have the opportunity to work across various sectors in the city, from corporate offices to tech companies, and from legal practices to design studios. This variety ensures every day offers something different.</p>
+
+<p><strong>We're proud to offer competitive compensation, flexible scheduling, comprehensive training, and a supportive work environment.</strong> Whether you're an experienced cleaner looking for a new challenge or someone interested in starting a new career in commercial cleaning, we have a place for you.</p>
+
+<p><b class="highlight">Start your journey with us today.</b> Explore our office cleaning jobs in London and apply to become part of London's leading commercial cleaning company. We look forward to welcoming you to our network where high standards, reliability, and trust form the backbone of our success.</p>
+
+<p><b>To apply, please fill out the form below.</b> We can't wait to learn more about you!</p>`
+
+
+
+
+
+// LOCATIONS PAGE
+
+    // META
+
+export const LOCATIONS_TITLE = 'Our Locations · The Clean Network'
+
+export const LOCATIONS_DESCRIPTION = "INSERT CONTENT"
+
+export const LOCATIONS_KEYWORDS = "INSERT CONTENT"
+
+
+    // CONTENT
+
+export const LOCATIONS_H1 = "Our Locations"
+
+export const LOCATIONS_INTRO_DESCRIPTION = "Centrally located in the <b>City of London</b>, we're just a call away from providing your space with the professional cleaning service it deserves. <b>We’d be happy to come over and provide you with a quote.</b> Or feel free to drop by our office too."
+
+
+
+
+
+
+// FOOTER
+
+export const FOOTER_DESCRIPTION = "Our ethos is to provide high standards of cleaning with strong account management to ensure smooth day to day running of your cleaning contract at an affordable price."
+
+
+
+
+
+
