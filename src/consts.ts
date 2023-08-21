e587e6a636f4fe33ad736a40997a28ea6456c4b2
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2023-08-21 21:56:11 +0100

    Add files via upload

---
+++ src/consts.ts
@@ -0,0 +1,283 @@
+// GENERAL
+
+export const FULL_ADDRESS = "6-8 Knights Court, London, EC1M 4NH"
+
+export const STREET = "6-8 Knights Court"
+
+export const LONDON_PHONE_NO = "020 3411 0275"
+
+export const LONDON_PHONE_NO_HREF = "+44-020-3411-0275"
+
+
+
+
+// HOME PAGE
+
+    // META
+
+export const SITE_TITLE = 'Professional Commercial & Office Cleaning in London - The Clean Network';
+
+export const SITE_DESCRIPTION = "The Clean Network, London's trusted partner in professional commercial and office cleaning. Reliable, high standards, tailored to your needs. See why 95% of our clients stay.";
+
+export const SITE_KEYWORDS = "commercial cleaning, office cleaning, London, services, company, professional, specialist, best, commercial/office cleaning services, London, corporate office buildings, cleaning services, London small offices, cleaning services, London medical and dental offices, cleaning services, London architects offices, cleaning services, London media agencies and designers offices, cleaning services, London legal practices, cleaning services, London technology companies, cleaning services, London financial service companies, cleaning services, London"
+
+
+    // CONTENT
+
+export const SITE_H1 = 'Commercial & Office Cleaning in London'
+
+export const INTRO_H2 = 'Welcome to <strong>The Clean Network</strong> - trusted by over 500 offices in London each day. Discover the difference our professional office cleaning services can make. <b>Why do 95% of our clients renew their contracts with us? <span class="highlight">Let us show you.</span></b>'
+
+export const TEXT_BLOCK = "We're more than a cleaning company. <b>We're your partners in creating a clean, healthy, and productive work environment.</b> Our professional team caters to various sectors in London, ensuring your specific needs are met with meticulous attention to detail, unmatched reliability, and high industry standards. <b>Experience the professionalism that keeps our clients coming back.</b>"
+
+export const SERVICES_SECTION_DESCRIPTION = "At <strong>The Clean Network</strong>, we offer a comprehensive range of services. From offices to commercial spaces, our team is equipped with the latest technology and training to deliver impeccable results."
+
+export const SECTORS_SECTION_DESCRIPTION = "<b>We understand that each place has unique needs.</b> That's why we specialise in providing tailored cleaning solutions to various sectors."
+
+export const LOCATIONS_SECTION_DESCRIPTION = "Centrally located in the City of London, we're just a call away from providing your space with the professional cleaning service it deserves. We’d be happy to come over and provide you with a quote. Or feel free to drop by our office too."
+
+// END OF HOME PAGE
+
+
+
+
+
+
+
+// SOCIAL SHARING IMAGE PATH
+
+export const OG_IMAGE = '/og/social-image.jpg'
+
+
+
+
+
+
+
+// SERVICES
+
+    // META
+
+export const SERVICES_TITLE = 'Commercial & Office Cleaning Services in London - The Clean Network'
+
+export const SERVICES_DESCRIPTION = 'Explore a wide range of professional commercial and office cleaning services by The Clean Network. Tailored solutions to meet your specific cleaning needs in London.'
+
+export const SERVICES_KEYWORDS = ""
+
+
+    // CONTENT
+
+export const SERVICES_H1 = "Commercial & Office Cleaning Services"
+
+export const SERVICES_INTRO_DESCRIPTION = "<strong>Discover the breadth and depth of our cleaning expertise at The Clean Network.</strong> We offer a comprehensive range of interior and exterior commercial and office <strong>cleaning services in London</strong>, tailor-made to meet your unique needs and exceed your expectations."
+
+export const INTERIOR_SERVICES_DESCRIPTION = "Our interior cleaning services are meticulously designed to ensure a clean, hygienic, and productive workspace."
+
+export const EXTERIOR_SERVICES_DESCRIPTION = "We extend our cleaning expertise beyond your building's interior."
+
+
+
+
+
+
+
+// SECTORS
+
+    // META
+
+export const SECTORS_TITLE = 'Sectors We Work With - The Clean Network'
+
+export const SECTORS_DESCRIPTION = 'Discover the range of commercial & office cleaning services offered by The Clean Network in London. Comprehensive, reliable, professional. Your sector, our expertise.'
+
+export const SECTORS_KEYWORDS = ""
+
+
+    // CONTENT
+
+export const SECTORS_H1 = "Commercial & Office Cleaning Services in London"
+
+export const SECTORS_INTRO_DESCRIPTION = "<strong>The Clean Network</strong> is here for all your commercial and office cleaning needs in London. We take pride in our range of professional cleaning services, all <b>tailored to provide a clean, healthy, and productive work environment that leaves a lasting impression</b>"
+
+
+
+
+
+
+
+
+
+// CONTACT PAGE
+
+export const CONTACT_TITLE = 'Contact Us · The Clean Network'
+
+export const CONTACT_DESCRIPTION = ""
+
+export const CONTACT_KEYWORDS = ""
+
+
+
+
+
+
+
+// ABOUT US PAGE
+
+    // META
+
+export const ABOUT_TITLE = 'About The Clean Network - Who We Are'
+
+export const ABOUT_DESCRIPTION = "We are one of the leading office cleaning companies in London. The Clean Network is your professional partner for commercial and office cleaning in London"
+
+export const ABOUT_KEYWORDS = ""
+
+
+    // CONTENT
+
+export const ABOUT_H1 = "About <strong>The Clean Network</strong>"
+
+export const ABOUT_H2 = "The Clean Network – Our Story, Our Values, Our Commitment"
+    
+export const ABOUT_TEXT_BODY = `<p>We are one of the leading office <strong>cleaning companies in London</strong>.</p>
+    
+<p>The Clean Network is your professional partner for commercial and office cleaning in London. Our story began with a singular mission - to provide businesses with <strong>reliable, high-standard cleaning services, instilling trust through our actions and maintaining a level of quality that speaks for itself.</strong></p>
+
+<p>Our philosophy is simple yet robust - cleanliness and productivity go hand in hand. A clean working environment not only enhances the well-being of your staff but also promotes efficiency, motivation, and a positive corporate image. <b>We have spent years perfecting our services and processes, continuously investing in our team to meet the evolving needs of businesses in London.</b></p>
+
+<p>Today, we are proud to be the trusted choice of over <b>500 offices daily, from small offices to corporate office buildings, legal practices to technology companies, and more.</b> Our cleaning specialists understand the unique needs of each sector, delivering tailored services with meticulous attention to detail and the <b>highest standards</b>.</p>
+
+<p>At The Clean Network, we believe in building strong, long-lasting relationships with our clients. <b>Our 95% contract renewal rate speaks volumes about the trust businesses place in our services and our consistent ability to deliver excellence.</b> We don’t just clean; we care for your workspace as if it were our own, bringing an unwavering commitment to cleanliness and hygiene to your door, every single day.</p>
+
+<p>Our team is our strength. Comprising skilled, dedicated professionals passionate about delivering top-tier service, we ensure every member of our team is thoroughly trained and equipped with the latest cleaning technology. As a company, <b>we are committed to providing a positive, supportive work environment where our team can excel</b>, reflecting our commitment to excellence in every task we undertake.</p>
+
+<p><strong class="highlight">Partner with us and discover the difference The Clean Network can make.</strong></p>`
+
+
+
+// JOBS PAGE
+
+    // META
+
+export const JOBS_TITLE = 'Office Cleaning Jobs in London - The Clean Network'
+
+export const JOBS_DESCRIPTION = "Looking for office cleaning jobs in London? Join The Clean Network, London's leading commercial cleaning company. Explore our job opportunities near you. Apply now!"
+
+export const JOBS_KEYWORDS = "cleaning job offers, cleaning work, cleaning job in london"
+
+
+    // CONTENT
+
+export const JOBS_H1 = "Office Cleaning Jobs"
+
+export const JOBS_TEXT_BODY = `<p>If you're seeking a rewarding career in commercial cleaning, you're in the right place. <strong>Based in London, we're proud to offer a range of job opportunities in office cleaning, right in your area.</strong></p>
+    
+<p>At <strong>The Clean Network</strong>, we firmly believe that our team is our strength, and we're always on the lookout for dedicated individuals who share our <b>commitment to cleanliness, professionalism, and customer service.</b> As part of our team, you'll have the opportunity to work across various sectors in the city, from corporate offices to tech companies, and from legal practices to design studios. This variety ensures every day offers something different.</p>
+
+<p><strong>We're proud to offer competitive compensation, flexible scheduling, comprehensive training, and a supportive work environment.</strong> Whether you're an experienced cleaner looking for a new challenge or someone interested in starting a new career in commercial cleaning, we have a place for you.</p>
+
+<p><b class="highlight">Start your journey with us today.</b> Explore our office cleaning jobs in London and apply to become part of London's leading commercial cleaning company. We look forward to welcoming you to our network where high standards, reliability, and trust form the backbone of our success.</p>
+
+<p><b>To apply, please fill out the form below.</b> We can't wait to learn more about you!</p>`
+
+
+
+
+
+// LOCATIONS PAGE
+
+    // META
+
+export const LOCATIONS_TITLE = 'Our Location · The Clean Network'
+
+export const LOCATIONS_DESCRIPTION = ""
+
+export const LOCATIONS_KEYWORDS = ""
+
+
+    // CONTENT
+
+export const LOCATIONS_H1 = "Our Location"
+
+export const LOCATIONS_INTRO_DESCRIPTION = "Centrally located in the <b>City of London</b>, we're just a call away from providing your space with the professional cleaning service it deserves. <b>We’d be happy to come over and provide you with a quote.</b> Or feel free to drop by our office too."
+
+
+
+
+
+
+// FOOTER
+
+export const FOOTER_DESCRIPTION = "Our ethos is to provide high standards of cleaning with strong account management to ensure smooth day to day running of your cleaning contract at an affordable price."
+
+
+
+
+
+
+// PRIVACY POLICY
+
+    // META
+    
+export const PRIVACY_TITLE = 'Privacy Policy - The Clean Network'
+
+export const PRIVACY_DESCRIPTION = 'This policy relates to the processing of personal data as undertaken by The Clean Network, the trading name of The Clean Network Limited.'
+
+export const PRIVACY_KEYWORDS = "Privacy Policy, Clean Network"
+
+
+
+    // CONTENT
+
+export const PRIVACY_H1 = "Privacy Policy"
+
+export const PRIVACY_TEXT = `
+<p><b>The Clean Network</b> is a registered business no: <b>09308063</b> registered office <b>12 Hatherley Road, Sidcup, Kent, DA14 4DT</b></p>
+
+<p>This privacy policy sets out how The Clean Network Limited uses and protects any personal information.</p>
+
+<p>We are committed to ensuring that your privacy is protected and that we collect personal information fairly and transparently. Should we ask you to provide certain information by which you can be identified, then you can be assured that will only be using it in accordance with this privacy statement. We may change policy from time to time and will update this policy accordingly.</p>
+
+<h2>What information is collected?</h2>
+
+<p>We may collect the following information:</p>
+
+<p>Information relating to your position in your company, for example your job title, place of work and role</p>
+
+<p>If you are a company director, we may also process data relating to your directorship that is in the public domain</p>
+
+<p>If you make an enquiry about our products or services whether via our website, telephone or email we will collect information about you in order to respond to your enquiry</p>
+
+<h2>Contact information including email address</h2>
+
+<p>Demographic information such as postcode, preferences and interests</p>
+<p>Other information relevant to your employment with and/or relationship with All Square Limited.</p>
+
+<h2>The information we collect allows us to:</h2>
+
+<p>Communicate effectively with you when responding to enquiries
+Provide you with information on other products and services if you choose to receive such information</p>
+
+<p>Provide our services to you based on accurate information</p>
+<p>Maintain accurate and concise internal records</p>
+<p>Comply with HR, Financial and legal requirements</p>
+<p>For All Square Limited employees, we may periodically send important information that may affect your day to day job and other company matters and announcements.</p>
+
+<h2>How we use cookies</h2>
+
+<p>A cookie is a small file which asks permission to be placed on your computer’s hard drive. Once you agree, the file is added, and the cookie helps analyse web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences.</p>
+
+<p>We use traffic log cookies to identify which pages are being used. This helps us analyse data about webpage traffic and improve our website in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system.</p>
+
+<p>Overall, cookies help us provide you with a better website by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us.</p>
+
+<p>You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. This may prevent you from taking full advantage of the website.</p>
+
+<h2>Disclosing your information</h2>
+
+<p>In line with <b>GDPR Article 6(1)(c)</b>, we may pass on your personal information and exchange any of your details if we have a legal obligation to do so, or in order to enforce or apply our contract terms and conditions or other agreements.</p>
+
+<h2>Controlling your personal information</h2>
+
+<p>You may request details of personal information which we hold about you under the General Data Protection Regulation (GDPR) and ask. If you would like a copy of the information held on you please write to <b>The Clean Network Limited, Floor 3, 6-8 Bonhill Street London EC2A 4BX</b>.</p>
+
+<p>If you believe that any information we are holding on you is incorrect or incomplete, please write to or email us as soon as possible at office@cleannetwork.co.uk. We will promptly correct any information found to be incorrect.</p>
+`
