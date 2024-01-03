966c1fe56e68b0ad9e6246a93be96c5b1882bd87
Dex REAU <dev@oqva.digital>
2024-01-03 13:46:25 -0300

    Update consent.js

--- src/scripts/consent.js
+++ src/scripts/consent.js
@@ -48,3 +48,20 @@
         localStorage.setItem('consentMode', JSON.stringify(consentMode));
     }
     
+
+// Below added from Christina's Cleaners, consent modal will work on localhost
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+if(localStorage.getItem('consentMode') === null){
+    gtag('consent', 'default', {
+        'ad_storage': 'denied',
+        'analytics_storage': 'denied',
+        'personalization_storage': 'denied',
+        'functionality_storage': 'denied',
+        'security_storage': 'denied',
+    });
+} else {
+    gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')));
+}
